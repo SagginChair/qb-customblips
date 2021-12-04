e734c814132130fcc9e78cb537ef0aba6fb09d81
SagginChair <Vlezxsaggin1@gmail.com>
2021-12-04 01:55:37 -0600

    Update blips.lua

--- client/blips.lua
+++ client/blips.lua
@@ -10,6 +10,7 @@ local blips = {
   {title="Boostin", colour=30 , id=620,x = 485.36, y = -3383.1, z = 0},
   {title="Weed Depot", colour=62 , id=140,x = -1172.088, y = -1571.833, z = 0},
   {title="Car Rentals", colour=70 , id=290,x = 181.19, y = -1010.28, z = 0},
+  {title="Strip Club", colour=61 , id=121,x = 126.87, y = -1297.25, z = 0},
  }
  
  -- To add additional function to the below blip display, you can add natives to change/add/remove various things, just go here (https://docs.fivem.net/natives/) and search 'SetBlip' and it will give you a list. Example: https://docs.fivem.net/natives/?_0xB14552383D39CE3E
