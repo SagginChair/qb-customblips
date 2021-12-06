61850f80037c3447c351f8754dbd14bf52496775
SagginChair <Vlezxsaggin1@gmail.com>
2021-12-06 17:09:35 -0600

    Update blips.lua

--- client/blips.lua
+++ client/blips.lua
@@ -2,11 +2,8 @@
 -- https://docs.fivem.net/docs/game-references/blips/
 
 local blips = {
-  {title="Vineyard", colour=58 , id=615,x = -1928.9, y = 2048.9, z = 0},
   {title="Oxy Delivery", colour=2 , id=403,x = 69.0, y = -1570.05, z = 0},
   {title="Apple Store", colour=22 , id=590,x = 154.59, y = -219.06, z = 0},
-  {title="Electric Fuse", colour=1 , id=354,x = 379.74, y = -907.84, z = 0},
-  {title="Electric Fuse", colour=1 , id=354,x = 158.27, y = -224.37, z = 0},
   {title="Boostin", colour=30 , id=620,x = 485.36, y = -3383.1, z = 0},
   {title="Weed Depot", colour=62 , id=140,x = -1172.088, y = -1571.833, z = 0},
   {title="Car Rentals", colour=70 , id=290,x = 181.19, y = -1010.28, z = 0},
